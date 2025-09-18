58b4369a972cb41918b734bc72b1a93dabc2dae4
Siberiaaa <siberiamvg@gmail.com>
2025-09-17 22:56:52 -0400

    chore: add initial app template with views, base styles, commit hooks, and eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,6 +21,14 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "unused-imports/no-unused-imports": "error",
+  }
   },
 ];
 
