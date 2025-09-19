ec3c11831d37c9777fba596d128613ac712eb994
Siberiaaa <siberiamvg@gmail.com>
2025-09-19 09:40:53 -0400

    feat: add react context, sweetalert2, and crud ui for client and event types without db integration

---
+++ src/app/(DashboardLayout)/event-types/page.tsx
@@ -0,0 +1,135 @@
+'use client';
+import { useState } from 'react';
+import { Box, Table, TableHead, TableBody, TableRow, TableCell, Typography, Dialog, DialogTitle, DialogContent, Button, TextField } from '@mui/material';
+import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
+import DashboardCard from '@/app/(DashboardLayout)/components/shared/DashboardCard';
+import CircularProgress from '@mui/material/CircularProgress';
+import { showSucessAlert } from "@/lib/swal";
+
+const ClientTypesPage = () => {
+  const products = [
+    {
+      id: '1',
+      name: 'Tipo de evento 1',
+      description: 'Tipo de cliente 1 descripcion',
+    },
+    {
+      id: '2',
+      name: 'Tipo de evento 2',
+      description: 'Tipo de cliente 2 descripcion',
+    },
+    {
+      id: '3',
+      name: 'Tipo de evento 3',
+      description: 'Tipo de cliente 13 descripcion',
+    },
+  ];
+
+  const [openModal, setOpenModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const [selectedType, setSelectedType] = useState<{ id: string; name: string; description: string } | null>(null);
+
+  // open modal
+  const handleRowClick = (type: { id: string; name: string; description: string }) => {
+    setSelectedType(type);
+    setOpenModal(true);
+  };
+
+  // close modal
+  const handleClose = () => {
+    setOpenModal(false);
+  };
+
+   const handleModify = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setLoading(true);
+    const formData = new FormData(event.currentTarget);
+
+    const updated = {
+      id: formData.get("id") as string,
+      name: formData.get("name") as string,
+      description: formData.get("description") as string,
+    };
+
+    
+    // some modify logic
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // para que se awaitee correctamente
+
+    console.log("modified info:", updated);
+    showSucessAlert("event type modified successfully");
+    //
+
+    setLoading(false);
+    setOpenModal(false);
+  };
+
+  return (
+    <PageContainer title='Event Types' description='Event Types Page'>
+      <DashboardCard title='Event Types'>
+        <Box sx={{ overflow: 'auto', width: { xs: '280px', sm: 'auto' } }}>
+          <Table aria-label='event table' sx={{ whiteSpace: 'nowrap', mt: 2 }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <Typography variant='subtitle2' fontWeight={600}>
+                    ID
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant='subtitle2' fontWeight={600}>
+                    Name
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products.map((type) => (
+                <TableRow
+                  key={type.name}
+                  className='cursor-pointer hover:bg-indigo-100 active:bg-indigo-200'
+                  onClick={() => {
+                    handleRowClick(type);
+                  }}
+                >
+                  <TableCell>
+                    <Typography sx={{ fontSize: '15px', fontWeight: '500' }}>{type.id}</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography sx={{ fontSize: '15px', fontWeight: '600' }}>{type.name}</Typography>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+      </DashboardCard>
+
+      {/* modal */}
+      <Dialog open={openModal} onClose={handleClose} maxWidth='sm' fullWidth>
+        <DialogTitle>Modify client type</DialogTitle>
+        <DialogContent dividers>
+          {selectedType && (
+            <Box component='form' onSubmit={handleModify} display='flex' flexDirection='column' gap={2} mt={1}>
+              <TextField label='ID' name='id' defaultValue={selectedType.id} disabled />
+              <TextField label='Name' name='name' defaultValue={selectedType.name} required/>
+              <TextField label='Description' name='description' defaultValue={selectedType.description} required/>
+              <Box display="flex" justifyContent="center" gap={2}>
+                <Button variant='contained' type='submit' color='primary' disabled={loading}>
+                  Modify
+                  {loading && <CircularProgress size="15px" className={'ml-2'} />}
+                </Button>
+                <Button onClick={handleClose} color='secondary' disabled={loading}>
+                  Cancel
+                </Button>
+              </Box>
+            </Box>
+          )}
+        </DialogContent>
+      </Dialog>
+    </PageContainer>
+  );
+};
+
+export default ClientTypesPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { baselightTheme } from "@/utils/theme/DefaultColors";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { AppProvider } from '@/context/AppContext';
 import './global.css'
 
 
@@ -13,11 +14,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ThemeProvider theme={baselightTheme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          {children}
-        </ThemeProvider>
+        <AppProvider>
+          <ThemeProvider theme={baselightTheme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppProvider>
       </body>
     </html>
   );
