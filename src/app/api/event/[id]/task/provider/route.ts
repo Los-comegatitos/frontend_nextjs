117bc5af1f0906451bc8b0fa0ee27406a8f2a0cb
david_mendoza <mendozadavidprogramacion2.0@gmail.com>
2025-10-10 19:00:10 -0400

    feat: visual task

--- src/app/api/event/[id]/task/provider/route.ts
+++ src/app/api/event/[id]/task/provider/route.ts
@@ -2,13 +2,30 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { API_BACKEND } from '@/app/lib/definitions';
 
+// Listar tareas de un proveedor
+export async function GET(
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  try {
+    const token = req.headers.get('token');
+    const { id } = await context.params;
 
-//listar tareas de un proveedor
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-  const token = req.headers.get('token');
-  const res = await fetch(`${API_BACKEND}events/${params.id}/tasks/provider`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  const data = await res.json();
-  return NextResponse.json(data, { status: res.status });
+    if (!id) {
+      return NextResponse.json({ message: 'Missing id' }, { status: 400 });
+    }
+
+    const res = await fetch(`${API_BACKEND}events/${id}/tasks/provider`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const data = await res.json();
+    return NextResponse.json(data, { status: res.status });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: { code: '999', description: 'Error interno' } },
+      { status: 500 }
+    );
+  }
 }
