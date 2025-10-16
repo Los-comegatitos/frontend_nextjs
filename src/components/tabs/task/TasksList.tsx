79bc38c1efd4c1f1e553444d7150f4bb4c8cb0b6
Siberiaaa <siberiamvg@gmail.com>
2025-10-15 22:17:30 -0400

    feat: task calendar, dashboard event

--- src/components/tabs/task/TasksList.tsx
+++ src/components/tabs/task/TasksList.tsx
@@ -1,16 +1,11 @@
 'use client';
-import {
-  Box,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Typography,
-} from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Table, TableBody, TableCell, TableHead, TableRow, Typography, Card } from '@mui/material';
 import Image from 'next/image';
 import { Task } from '@/interfaces/Task';
-
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import esLocale from '@fullcalendar/core/locales/es';
 
 type Props = {
   tasks: Task[];
@@ -20,11 +15,47 @@ type Props = {
   onComment: (task: Task) => void;
 };
 
+interface TaskCalendar {
+  title: string;
+  date: string;
+  color: string;
+  task: Task,
+}
 
 export default function TaskList({ tasks, onAdd, onView, onComment }: Props) {
+  const [showCalendar, setShowCalendar] = useState(false);
+  const eventsArray: TaskCalendar[] =
+    tasks.flatMap((task) => [
+      {
+        title: task.name,
+        date: task.reminderDate.split('T')[0],
+        color: '#439fbf',
+        task:task,
+      },
+      {
+        title: task.name,
+        date: task.dueDate.split('T')[0],
+        color: '#491d8d',
+        task:task,
+      },
+    ]) || [];
+
   return (
     <Box>
-      <Box display="flex" justifyContent="flex-end" mb={2}>
+      <Box display='flex' justifyContent='flex-end' mb={2} gap={1}>
+        <button
+          onClick={() => setShowCalendar((selected) => !selected)}
+          style={{
+            backgroundColor: '#439fbf',
+            color: 'white',
+            padding: '6px 12px',
+            borderRadius: '4px',
+            border: 'none',
+            cursor: 'pointer',
+          }}
+        >
+          {showCalendar ? 'Lista' : 'Calendario'}
+        </button>
         <button
           onClick={onAdd}
           style={{
@@ -36,11 +67,55 @@ export default function TaskList({ tasks, onAdd, onView, onComment }: Props) {
             cursor: 'pointer',
           }}
         >
-          ➕ Añadir tarea
+          Añadir tarea
         </button>
       </Box>
 
-      {tasks.length === 0 ? (
+      {showCalendar ? (
+        <Card
+          sx={{
+            height: '100%',
+            padding: 4,
+            // esta cosa rara es para el pointer
+            '& .fc-event-main': {
+              cursor: 'pointer',
+            },
+          }}
+        >
+          <FullCalendar
+            locale={esLocale}
+            plugins={[dayGridPlugin]}
+            initialView='dayGridMonth'
+            events={eventsArray}
+            eventClick={(info) => {
+              onView(info.event.extendedProps.task);
+            }}
+          />
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              gap: 3,
+              mt: 2,
+              alignItems: 'center',
+            }}
+          >
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <Box sx={{ width: 14, height: 14, bgcolor: '#439fbf', borderRadius: 0.5 }} />
+              <Typography variant='body2' color='text.secondary'>
+                Recordatorio
+              </Typography>
+            </Box>
+
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <Box sx={{ width: 14, height: 14, bgcolor: '#491d8d', borderRadius: 0.5 }} />
+              <Typography variant='body2' color='text.secondary'>
+                Fecha límite
+              </Typography>
+            </Box>
+          </Box>
+        </Card>
+      ) : tasks.length === 0 ? (
         <Typography>No hay tareas registradas.</Typography>
       ) : (
         <Table>
@@ -49,7 +124,7 @@ export default function TaskList({ tasks, onAdd, onView, onComment }: Props) {
               <TableCell>Nombre</TableCell>
               <TableCell>Descripción</TableCell>
               <TableCell>Estado</TableCell>
-              <TableCell align="center">Acciones</TableCell>
+              <TableCell align='center'>Acciones</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -58,7 +133,7 @@ export default function TaskList({ tasks, onAdd, onView, onComment }: Props) {
                 <TableCell onClick={() => onView(task)}>{task.name}</TableCell>
                 <TableCell onClick={() => onView(task)}>{task.description}</TableCell>
                 <TableCell onClick={() => onView(task)}>{task.status}</TableCell>
-                <TableCell align="center">
+                <TableCell align='center'>
                   <button
                     onClick={() => onComment(task)}
                     style={{
@@ -67,12 +142,7 @@ export default function TaskList({ tasks, onAdd, onView, onComment }: Props) {
                       cursor: 'pointer',
                     }}
                   >
-                    <Image
-                      src="/images/icons/message_9351720.png"
-                      alt="Comentarios"
-                      width={24}
-                      height={24}
-                    />
+                    <Image src='/images/icons/message_9351720.png' alt='Comentarios' width={24} height={24} />
                   </button>
                 </TableCell>
               </TableRow>
